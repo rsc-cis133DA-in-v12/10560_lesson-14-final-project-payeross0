0b4a02fca1fce292a425d19688376cd5c12a81f4
Payeton Ross <pay2162183@maricopa.edu>
2024-03-09 20:02:45 -0700

    Completed final touch-ups and is ready for review.

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/* 
+    Author: Payeton Ross
+    Course: CIS133DA 10560
+    Lesson: 14
+*/
+// Function to alert user the form has been submitted
+function formSubmit() {
+    alert('Your form has been submitted.')
+}
+
+// Function to reset the form for the user
+function formReset() {
+    document.getElementById("resetMessage").style.visibility = "visible";
+}
